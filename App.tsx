a1f930b9ef96e5f29f8fe49b9ba20b20e0c6182b
Igor Hernandez <igorhernandez.dev@hotmail.com>
2022-04-02 12:42:51 -0300

    feat: handle get pokemons on home screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,51 @@
-import { Text, SafeAreaView } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
+import { useEffect, useState } from 'react'
+import { Text, SafeAreaView, FlatList } from 'react-native'
+import { getPokemons } from './src/services'
+
+interface ISpecies {
+  color: { name: string; url: string }
+  egg_groups: Array<{ name: string; url: string }>
+}
+
+interface IPokemon {
+  id: number
+  name: string
+  species: ISpecies
+  url: string
+}
 
 export default function App() {
+  const [pokemons, setPokemons] = useState<Array<IPokemon>>([
+    {
+      id: 0,
+      name: '',
+      species: {
+        color: { name: '', url: '' },
+        egg_groups: [{ name: '', url: '' }]
+      },
+      url: ''
+    }
+  ])
+
+  const handleGetPokemons = async () => {
+    const pokemonsResponse = await getPokemons()
+    if (!pokemonsResponse) return null
+    setPokemons(pokemonsResponse)
+  }
+
+  useEffect(() => {
+    handleGetPokemons()
+  }, [])
+
   return (
     <SafeAreaView>
+      <FlatList
+        data={pokemons}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+        keyExtractor={(item) => item.name}
+      />
       <StatusBar style="auto" />
-      <Text>Pokedex</Text>
     </SafeAreaView>
   )
 }
