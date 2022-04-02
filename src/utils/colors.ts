4df4fb813a2d83694382dc9607f815ec69852cf6
Igor Hernandez <igorhernandez.dev@hotmail.com>
2022-04-02 20:27:28 -0300

    feat: added colors file

---
+++ src/utils/colors.ts
@@ -0,0 +1,31 @@
+export const colors = {
+  black: '#2b2b2b',
+  blue: '#6897c7',
+  softBlue: '#92b5e1',
+  lightBlue: '#2db0ef',
+  brown: '#cb9393',
+  lightBrown: '#ec8583',
+  gray: 'gray',
+  green: '#93cba8',
+  darkGreen: '#3fb7af',
+  lightGreen: '#35ac58',
+  pink: 'pink',
+  purple: '#936cc3',
+  red: '#f3cca1',
+  white: 'white',
+  yellow: '#c3bc6c',
+  orange: '#ec713d'
+}
+
+export const treatColors = {
+  black: colors.black,
+  blue: colors.blue,
+  brown: colors.brown,
+  gray: colors.gray,
+  green: colors.green,
+  pink: colors.pink,
+  purple: colors.purple,
+  red: colors.red,
+  white: '#ccc',
+  yellow: colors.yellow
+}
