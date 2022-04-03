97d1320026e1edea1a9061c2539d906095d5be53
Igor Hernandez <igorhernandez.dev@hotmail.com>
2022-04-03 05:28:40 -0300

    feat: added navigation types to app

--- App.tsx
+++ App.tsx
@@ -1,8 +1,17 @@
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, RouteProp } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Screens } from './src/navigation/screenNames'
 import Detail from './src/screens/Detail/Detail'
 import Home from './src/screens/Home/Home'
+import { IPokemon } from './src/interfaces/pokemon.interfaces'
+
+export type RootStackParamList = {
+  Home: undefined
+  Detail: { pokemon: IPokemon }
+}
+
+export type RootRouteProps<RouteName extends keyof RootStackParamList> =
+  RouteProp<RootStackParamList, RouteName>
 
 export default function App() {
   const Stack = createNativeStackNavigator()
