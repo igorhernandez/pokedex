77acb62794242bfb6697231ba7753962896317bb
Igor Hernandez <igorhernandez.dev@hotmail.com>
2022-04-04 02:32:23 -0300

    feat: improve to a separeted backButton component

---
+++ src/components/BackButton/BackButton.tsx
@@ -0,0 +1,16 @@
+import { TouchableOpacity } from 'react-native'
+import { FontAwesome5 } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import { colors } from '../../utils/colors'
+
+const BackButton = ({ color = colors.white }) => {
+  const navigation = useNavigation()
+
+  return (
+    <TouchableOpacity onPress={() => navigation.goBack()}>
+      <FontAwesome5 name="arrow-left" color={color} size={28} />
+    </TouchableOpacity>
+  )
+}
+
+export default BackButton
