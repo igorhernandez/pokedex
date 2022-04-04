ed8ecf016b7dd9ca97af5911c578929908c6844f
Igor Hernandez <igorhernandez.dev@hotmail.com>
2022-04-04 17:48:30 -0300

    feat: created appProvider

--- App.tsx
+++ App.tsx
@@ -5,37 +5,38 @@ import Detail from './src/screens/Detail/Detail'
 import Home from './src/screens/Home/Home'
 import { IPokemon } from './src/interfaces/pokemon.interfaces'
 import Pokedex from './src/screens/Pokedex/Pokedex'
+import AppProvider from './src/context/provider'
 
 export type RootStackParamList = {
-  Home: undefined
-  Detail: { pokemon: IPokemon }
+  [Screens.HOME]: undefined
+  [Screens.DETAIL]: { pokemon: IPokemon }
+  [Screens.POKEDEX]?: { pokemon: IPokemon }
 }
-
-export type RootRouteProps<RouteName extends keyof RootStackParamList> =
-  RouteProp<RootStackParamList, RouteName>
 
 export default function App() {
   const Stack = createNativeStackNavigator()
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={Screens.HOME}>
-        <Stack.Screen
-          name={Screens.HOME}
-          options={{ headerShown: false }}
-          component={Home}
-        />
-        <Stack.Screen
-          name={Screens.DETAIL}
-          options={{ headerShown: false, presentation: 'modal' }}
-          component={Detail}
-        />
-        <Stack.Screen
-          name={Screens.POKEDEX}
-          options={{ headerShown: false }}
-          component={Pokedex}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AppProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={Screens.HOME}>
+          <Stack.Screen
+            name={Screens.HOME}
+            options={{ headerShown: false }}
+            component={Home}
+          />
+          <Stack.Screen
+            name={Screens.DETAIL}
+            options={{ headerShown: false, presentation: 'modal' }}
+            component={Detail}
+          />
+          <Stack.Screen
+            name={Screens.POKEDEX}
+            options={{ headerShown: false }}
+            component={Pokedex}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AppProvider>
   )
 }
